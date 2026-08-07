8202dc1e4c2ad00b46d588fe47b93e1b7af9a66a
DERAPTER <egorxalikov@gmail.com>
2026-08-08 02:05:31 +0300

    MainScreen HeaderSection Pinned to top of the screen

--- bookshelf/Module/MainView/View/MainViewContent.swift
+++ bookshelf/Module/MainView/View/MainViewContent.swift
@@ -15,92 +15,102 @@ struct MainViewContent: View {
     //var books: [Book]
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            VStack {
-                
-                //MARK: HEADER
-                VStack(alignment: .leading, spacing: 25) {
-                    HStack {
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text("Добрый день")
+        
+        ZStack(alignment: .top) {
+            //MARK: HEADER
+            VStack(alignment: .leading) {
+                HStack {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Добрый день")
+                            .font(size: 14)
+                        Text(name)
+                            .font(type: .black, size: 16)
+                    }
+                    .foregroundStyle(.white)
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack(spacing: 10) {
+                            Image(systemName: "book.closed")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 14, height: 14)
+                            Text("добавить")
                                 .font(size: 14)
-                            Text(name)
-                                .font(type: .black, size: 16)
                         }
+                        .padding(.vertical, 7)
+                        .padding(.horizontal, 14)
+                        .background(.appOrange)
+                        .clipShape(Capsule())
                         .foregroundStyle(.white)
-                        
-                        Spacer()
-                        
-                        Button {
-                            
-                        } label: {
-                            HStack(spacing: 10) {
-                                Image(systemName: "book.closed")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 14, height: 14)
-                                Text("добавить")
-                                    .font(size: 14)
-                            }
-                            .padding(.vertical, 7)
-                            .padding(.horizontal, 14)
-                            .background(.appOrange)
-                            .clipShape(Capsule())
-                            .foregroundStyle(.white)
-                        }
-                        
                     }
                     
-                    BaseTextView(placeholder: "Поиск", text: $searchField)
-                        .font()
                 }
-                .padding(.horizontal, 30)
-            
-                //MARK: READ
-                VStack(alignment: .leading, spacing: 18) {
-                    
-                    Text("Читаю")
-                        .font(type: .bold, size: 22)
-                        .foregroundStyle(.white)
-                        .padding(.horizontal, 30)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 20) {
-                            Button {
-                                
-                            } label: {
-                                Image(.cover)
-                                    .resizable()
-                                    .frame(width: 143, height: 212)
-                                    .clipShape(.rect(cornerRadius: 5))
-                            }
-                            
-                            Button {
-                                
-                            } label: {
-                                Image(.cover)
-                                    .resizable()
-                                    .frame(width: 143, height: 212)
-                                    .clipShape(.rect(cornerRadius: 5))
-                            }
-                            
-                            Button {
-                                
-                            } label: {
-                                Image(.cover)
-                                    .resizable()
-                                    .frame(width: 143, height: 212)
-                                    .clipShape(.rect(cornerRadius: 5))
-                            }
-                        }
-                        .padding(.horizontal, 30)
-                    }
-                }
-                
             }
+            .padding(.horizontal, 30)
+            .zIndex(9)
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    //MARK: READ
+                    VStack(alignment: .leading, spacing: 25) {
+                        
+                        BaseTextView(placeholder: "Поиск", text: $searchField)
+                            .font()
+                            .padding(.horizontal, 30)
+                        
+                        VStack(alignment: .leading, spacing: 18) {
+                            
+                            Text("Читаю")
+                                .font(type: .bold, size: 22)
+                                .foregroundStyle(.white)
+                                .padding(.horizontal, 30)
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 20) {
+                                    Button {
+                                        
+                                    } label: {
+                                        Image(.cover)
+                                            .resizable()
+                                            .frame(width: 143, height: 212)
+                                            .clipShape(.rect(cornerRadius: 5))
+                                    }
+                                    
+                                    Button {
+                                        
+                                    } label: {
+                                        Image(.cover)
+                                            .resizable()
+                                            .frame(width: 143, height: 212)
+                                            .clipShape(.rect(cornerRadius: 5))
+                                    }
+                                    
+                                    Button {
+                                        
+                                    } label: {
+                                        Image(.cover)
+                                            .resizable()
+                                            .frame(width: 143, height: 212)
+                                            .clipShape(.rect(cornerRadius: 5))
+                                    }
+                                }
+                                .padding(.horizontal, 30)
+                            }
+                        }
+                        
+                    }
+                    
+                    
+                }
+            }
+            //.ignoresSafeArea()
+            .padding(.top, 80)
+            .background(Color.bgMain)
         }
-        //.ignoresSafeArea()
-        .background(Color.bgMain)
     }
 }
 
